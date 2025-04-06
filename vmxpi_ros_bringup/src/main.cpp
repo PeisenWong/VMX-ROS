c7b3110d0e3f090379f26183ad0250e89e56dab6
PeisenWong <wongpeisen@gmail.com>
2025-04-06 18:07:44 +0800

    dvd

--- vmxpi_ros_bringup/src/main.cpp
+++ vmxpi_ros_bringup/src/main.cpp
@@ -283,7 +283,7 @@ public:
                     << ", back " << target_rpm_back);
 
                 // Publish motor commands
-                // publish_motors();
+                publish_motors();
             }
             rate.sleep();
         }
