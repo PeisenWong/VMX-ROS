3f078fd8b26086bba19bf3649171889df84dc39e
PeisenWong <wongpeisen@gmail.com>
2025-04-06 18:17:41 +0800

    dvdv

--- vmxpi_ros_bringup/src/main.cpp
+++ vmxpi_ros_bringup/src/main.cpp
@@ -91,9 +91,9 @@ public:
     ros::Subscriber vel_sub;
 
     // PID controllers for each wheel (tuned gains are example values)
-    PID pid_left  = PID(0.001, 0.015, 0.001);
-    PID pid_right = PID(0.001, 0.015, 0.001);
-    PID pid_back  = PID(0.001, 0.015, 0.001);
+    PID pid_left  = PID(1.0, 1.5, 0.001);
+    PID pid_right = PID(1.0, 1.5, 0.001);
+    PID pid_back  = PID(1.0, 1.5, 0.001);
 
     Robot(ros::NodeHandle* nh) {
         set_m_speed = nh->serviceClient<vmxpi_ros::MotorSpeed>("titan/set_motor_speed");
