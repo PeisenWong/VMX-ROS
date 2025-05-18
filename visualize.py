ef3e75c12652f09dc2741c4324294a5c9e8595c0
PeisenWong <wongpeisen@gmail.com>
2025-05-18 18:25:25 +0800

    vdv

--- visualize.py
+++ visualize.py
@@ -8,16 +8,16 @@ df = pd.read_csv("/home/pi/rpm_log.csv")
 plt.figure(figsize=(12, 8))
 
 # LEFT Wheel
-plt.plot(df["time"], df["target_left"], linestyle='--', label="Target RPM - Left")
-plt.plot(df["time"], df["measured_left"], linestyle='-', label="Measured RPM - Left")
+plt.plot(df["timestamp"], df["target_left"], linestyle='--', label="Target RPM - Left")
+plt.plot(df["timestamp"], df["measured_left"], linestyle='-', label="Measured RPM - Left")
 
 # RIGHT Wheel
-plt.plot(df["time"], df["target_right"], linestyle='--', label="Target RPM - Right")
-plt.plot(df["time"], df["measured_right"], linestyle='-', label="Measured RPM - Right")
+plt.plot(df["timestamp"], df["target_right"], linestyle='--', label="Target RPM - Right")
+plt.plot(df["timestamp"], df["measured_right"], linestyle='-', label="Measured RPM - Right")
 
 # BACK Wheel
-plt.plot(df["time"], df["target_back"], linestyle='--', label="Target RPM - Back")
-plt.plot(df["time"], df["measured_back"], linestyle='-', label="Measured RPM - Back")
+plt.plot(df["timestamp"], df["target_back"], linestyle='--', label="Target RPM - Back")
+plt.plot(df["timestamp"], df["measured_back"], linestyle='-', label="Measured RPM - Back")
 
 # Labels and aesthetics
 plt.title("Target vs Measured RPM (PID Tracking)")
