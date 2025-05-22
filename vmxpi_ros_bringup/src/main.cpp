06eb0b7d59fa98cbdbdd31942c473b3148db9305
PeisenWong <wongpeisen@gmail.com>
2025-05-22 10:19:37 +0800

    vd

--- vmxpi_ros_bringup/src/main.cpp
+++ vmxpi_ros_bringup/src/main.cpp
@@ -273,7 +273,7 @@ public:
     }
 
     void controlLoop() {
-        ros::Rate rate(20); 
+        ros::Rate rate(100); 
         while (ros::ok()) {
             {
                 auto now = std::chrono::steady_clock::now();
